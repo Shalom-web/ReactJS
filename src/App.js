81c11f16d71c8534c637da9a760ce237e7d88163
Shalom-web <armstrongshalom1@gmail.com>
2023-03-01 10:05:58 +0100

    First ReactJS

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const title= 'Welcome To My Blog';
+  const likes = 1000;
+  const link = 'http://www.google.com';
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <div className="content">
+      <h1>{ title }</h1>
+      <p>{likes} times</p>
+      <a href={link}>Google Site</a>
+
+      <p>{Math.random() + 10}</p>
+     </div>
     </div>
   );
 }
